4c58195d7fec503da64126d87a0aa0f3720721fb
huayadlly <huayadlly@gmail.com>
2017-07-22 10:51:56 +0800

    开始kotlin；

---
+++ HelloKotlin/src/main/java/Main.java
@@ -0,0 +1,10 @@
+/**
+ * Created by huayadlly on 2017/7/22.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Main of Java ");
+    }
+}
---
+++ HelloKotlin/src/main/kotlin/cn/taike/kotlin/app.kt
@@ -0,0 +1,13 @@
+package cn.taike.kotlin
+
+/**
+ * Created by huayadlly on 2017/7/22.
+ */
+
+fun sum(a: Int, b: Int): Int {
+    return a + b
+}
+
+fun main(args: Array<String>) {
+    print(sum(3,5))
+}
