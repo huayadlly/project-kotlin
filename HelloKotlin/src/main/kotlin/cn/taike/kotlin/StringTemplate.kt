27be070ca8d9c4e6f2da462b3b2662ab73c635df
huayadlly <huayadlly@gmail.com>
2017-07-22 12:48:49 +0800

    循环，when;

--- HelloKotlin/src/main/kotlin/cn/taike/kotlin/StringTemplate.kt
+++ HelloKotlin/src/main/kotlin/cn/taike/kotlin/StringTemplate.kt
@@ -6,8 +6,21 @@ package cn.taike.kotlin
 
 fun main(args: Array<String>) {
 
-    strings()
+//    strings()
 
+//    println("两个数中的较大者是:${maxOf(55, 99)}")
+//    println("两个数中的较大者是2:${maxOf2(55, 101)}")
+
+    //循环
+//    loops()
+
+    //使用when
+    println("hello: ${describe("hello")}")
+    println("1: ${describe(1)}")
+    println("12:${describe(12)}")  //WHY?  12:this is not String type
+    println("1234L: ${describe(220L)}")
+    println("other: ${describe("other232323")}")
+    println("13.6: ${describe(13.6)}")
 }
 
 // 字符串模板
@@ -19,5 +32,44 @@ fun strings() {
     a = 999
     var s2 = "${s1.replace("is", "are")},but now is $a"
 
-    print(s2)
+    println(s2)
 }
+
+// 条件判断
+fun maxOf(a: Int, b: Int): Int {
+
+    if (a > b) {
+        return a
+    } else {
+        return b
+    }
+}
+
+fun maxOf2(a: Int, b: Int) = if (a > b) a else b
+
+//使用循环
+fun loops() {
+    val items = listOf("apple", "orange", "banana", "tomato")
+//    for (item in items) {
+//        println(item)
+//    }
+
+    for (index in items.indices) {
+        println("item at $index is ${items[index]}")
+    }
+
+    var index: Int = 0
+    while (index < items.size) {
+        println("(while loop)item at $index is ${items[index]}")
+        index++
+    }
+}
+
+fun describe(obj: Any): String =
+        when (obj) {
+            1 -> "parameter is Int 1"                   //输入类型为数组 1
+            "hello" -> "parameter is String of hello"   //输入类型为字符串
+            is Long -> "this is long type"
+            !is String -> "this is not String type"
+            else -> "nothing"
+        }
